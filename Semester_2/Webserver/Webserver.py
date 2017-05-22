037075bf5c16683e73a9701951a33f858476fcd2
Alexander Bilz <mail@alexbilz.com>
2017-05-22 14:46:33 +0200

    Bugfix Flask Server

--- Semester_2/Webserver/Webserver.py
+++ Semester_2/Webserver/Webserver.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 app = Flask("TEST")
 #Decorator defines route
 @app.route("/")
 def index():
-    return render_template("test.html")
+    return render_template("rechner.html")
 
 @app.route("/calc_bmi")
 def calc_bmi():
@@ -11,13 +11,14 @@ def calc_bmi():
     groesse = int(groesse)
     gewicht = request.args.get('gewicht')
     bmi = int(gewicht) / (float(groesse/100) ** 2)
-    img='/static/'
+
     if(bmi < 19):
-        img+="sausage.jpg"
+        img="sausage.jpg"
     elif(bmi>19 and bmi<25):
-        img+="nudeln.jpg"
+        img="nudeln.jpg"
     else:
-        img+="spinat.jpg"
+        img="spinat.jpg"
+    img = url_for('static', filename=img)
     return render_template("ergebnis.html", ergebnis=bmi, src=img)
 if __name__ == '__main__':
     app.run(debug=True)
